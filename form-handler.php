9d2cfb031cf457b8511310df0a1593e3f60455b7
unknown <1181101292@student.mmu.edu.my>
2023-01-07 14:29:31 +0800

    modified php file

--- form-handler.php
+++ form-handler.php
@@ -1,26 +1,27 @@
 <?php 
-$name = echo $_POST['name'];
-$visitor_email = echo $_POST['email'];
-$subject = echo $_POST['subject'];
-$message = echo $_POST['message'];
+// $name = echo $_POST['name'];
+// $visitor_email = echo $_POST['email'];
+// $subject = echo $_POST['subject'];
+// $message = echo $_POST['message'];
 
-$email_from = 'https://nishinodae.github.io/SGIS/';
+// $email_from = 'https://nishinodae.github.io/SGIS/';
 
-$email_subject = 'New Form Submission';
+// $email_subject = 'New Form Submission';
 
-$email_body = "User Name: $name.\n".
-              "User Email: $visitor_email.\n".
-              "Subject: $subject.\n".
-              "User Email: $message.\n";
+// $email_body = "User Name: $name.\n".
+//               "User Email: $visitor_email.\n".
+//               "Subject: $subject.\n".
+//               "User Email: $message.\n";
 
-$to = 'jiesyukri12@gmail.com';
+// $to = 'jiesyukri12@gmail.com';
 
-$headers = "From: $email_from \r\n";
+// $headers = "From: $email_from \r\n";
 
-$headers .= "Reply-To: $visitor_email \r\n";
+// $headers .= "Reply-To: $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
+// mail($to,$email_subject,$email_body,$headers);
 
-header("Location: contact.html")
+// header("Location: contact.html")
+echo '<script>alert("Email sent")</script>';
 
 ?>
